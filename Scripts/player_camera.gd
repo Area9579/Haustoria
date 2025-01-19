878e2f5f83681266ffb89e6f05976658128005f4
Phoenix Jordan <136138123+BirdOfAsh@users.noreply.github.com>
2025-01-19 11:00:57 -0600

    slightly adjusted camera and deleted  some redundant code

--- Scripts/player_camera.gd
+++ Scripts/player_camera.gd
@@ -9,10 +9,6 @@ func _ready() -> void:
 
 func  _input(event: InputEvent) -> void:
 	var mousePos = get_viewport().get_mouse_position()
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			if event.pressed:
-				pass
 	if event is InputEventMouseMotion:
 		adjustCamera(0.1,0.05,mousePos)
 
