6fd316dee065a63848bf81c03700dc56c253c86c
Phoenix Jordan <136138123+BirdOfAsh@users.noreply.github.com>
2025-01-18 22:28:03 -0600

    Added some comments for readability

--- playerControl.gd
+++ playerControl.gd
@@ -4,18 +4,10 @@ var clickPositionx
 var clickPositionz
 
 func _ready() -> void:
-	pass
-	moveTowardsMousePosition()
+	moveTowardsMousePosition() # intitiate the movement towards mouse position loop
 
 
-func  _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			if event.pressed:
-				pass
-
-
-func raycastOnMousePosition():
+func raycastOnMousePosition(): #makes a ray from the camera to a place in the 3D world at the mouse position
 	var cam = get_viewport().get_camera_3d()
 	var mousePos = get_viewport().get_mouse_position()
 	var stateInSpace = get_world_3d().direct_space_state
@@ -29,22 +21,22 @@ func raycastOnMousePosition():
 	return(resultingRay)
 
 
-func getMouseWorldPosition():
+func getMouseWorldPosition(): #returns a position in 3D space from the mouse position
 	var raycastResult = raycastOnMousePosition()
 	if raycastResult:
 		return raycastResult.position
 	return position
 
 
-func moveTowardsMousePosition():
+func moveTowardsMousePosition(): #moves the player towards the mouse position
 	await awaitTimer()
-	if (abs(position.x-getMouseWorldPosition().x) > 1.2):
+	if (abs(position.x-getMouseWorldPosition().x) > 1.2): #below two if statements check if mouse is within a certain limit, then otherwise move
 		position.x = move_toward(position.x,getMouseWorldPosition().x,0.2)
 	if (abs(position.z-getMouseWorldPosition().z) > 1.2):
 		position.z = move_toward(position.z,getMouseWorldPosition().z,0.2)
 
 
-func awaitTimer():
+func awaitTimer(): #timer used to dictate how long to wait before each movement
 	await get_tree().create_timer(0.03).timeout
 	moveTowardsMousePosition()
 	
--- player_camera.gd
+++ player_camera.gd
@@ -2,28 +2,27 @@ extends Camera3D
 
 var defaultZPos: float
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	defaultZPos = position.z
+	defaultZPos = position.z #sets this variable as a base of what the z position for the camera was
 
 
 func  _input(event: InputEvent) -> void:
 	var mousePos = get_viewport().get_mouse_position()
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			if event.pressed:
-				pass
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion: #adjusts the camera whenever you move the mouse
 		adjustCamera(0.5,0.15,mousePos)
 
 
 func adjustCamera(limitX,limitY,mousePos):
+	#both of these variables get the center of the screen for both the x and y axis in 2D
 	var centerX = get_viewport().size.x/2
 	var centerY = get_viewport().size.y/2
+	#both of these variables get a float based on the viewport size to divide the inputs by
+	#the limits are how far away the camera is allowed to go at the maximum
 	var divFactorX: float = ((centerX*2)/limitX)
 	var divFactorY: float = ((centerY*2)/limitY)
 	var newPosX: float
 	var newPosZ: float
+	#the below code just does some basic math to move the camera further based on how far away your mouse is to the border of the screen
 	if mousePos.x > centerX:
 		newPosX = ((mousePos.x-centerX)/divFactorX)*2
 		position.x = newPosX
