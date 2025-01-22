7d4bb230f3fe9d688898045f8a2bb177e3b1280d
Aria-Williams <159070680+Area9579@users.noreply.github.com>
2025-01-22 01:36:04 -0600

    Resolving Merge Conflict

--- Scripts/Boss/feet.gd
+++ Scripts/Boss/feet.gd
@@ -11,5 +11,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if attacking:
-		print("Feet attack")
+	pass
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,13 +1,49 @@
 extends CharacterBody3D
 
-var clickPositionx
-var clickPositionz
+@onready var dashCooldown: Timer = $CooldownTimer
+@onready var mouseCooldown: Timer = $MouseInputTimer
 
-const SPEED = 5.0 #where is this being used?
+const SPEED = 10.0
 const JUMP_VELOCITY = 4.5
+const BOUNCE_MULTIPLIER = 3.0
+
+var mouseInput: String
+var dashCombo = 1
+var accel = 1.0
 
 func _ready() -> void:
-	moveTowardsMousePosition() #initiates the mouse tracking loop
+	mouseInput = "Left Click"
+
+
+func _physics_process(delta: float) -> void:
+	#below code for jumping
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	#this code is for continuous
+	#if Input.is_action_pressed("Left Click"):
+		#var directionVector: Vector2 = getDirectionVector()
+		#slideTowardsMouse(directionVector,delta)
+	#else:
+		#accel = 1.0
+		#velocity.x = lerp(velocity.x,0.0,5 * delta)
+		#velocity.z = lerp(velocity.z,0.0,5 * delta)
+	
+	#this code is for impulse
+	if Input.is_action_just_pressed(mouseInput):
+		if dashCooldown.is_stopped() and (mouseCooldown.is_stopped() or mouseCooldown.time_left <= 0.15):
+			mouseCooldown.start()
+			slideTowardsMouse(delta)
+			changeMouseInput(mouseInput)
+			if dashCombo < 1.6:
+				dashCombo += 0.2
+	else:
+		decelerate(delta)
+	move_and_slide()
+	rebound()
 
 
 func raycastOnMousePosition(): #function that greates a raycast from the camera to a space in the 3D world based on the mouse position
@@ -31,28 +67,42 @@ func getMouseWorldPosition(): #gets a vector3 based on the camera raycast
 	return position
 
 
-func moveTowardsMousePosition(): #moves the player towards the position of the mouse
-	await awaitTimer()
-	if (abs(position.x-getMouseWorldPosition().x) > 1.2):
-		position.x = move_toward(position.x,getMouseWorldPosition().x,0.06)
-	if (abs(position.z-getMouseWorldPosition().z) > 1.2):
-		position.z = move_toward(position.z,getMouseWorldPosition().z,0.06)
+func getDirectionVector():
+	var mousePos = Vector2(getMouseWorldPosition().x,getMouseWorldPosition().z)
+	var playerVector2 = Vector2(position.x,position.z)
+	var degreeAngle = -rad_to_deg(playerVector2.angle_to_point(mousePos))
+	var directionVector = Vector2(cos(deg_to_rad(degreeAngle)),sin(deg_to_rad(degreeAngle)))
+	return directionVector
 
 
-func awaitTimer(): #this timer determens how frequently to call the player movement function
-	await get_tree().create_timer(0.004).timeout
-	moveTowardsMousePosition()
+func slideTowardsMouse(delta):
+	var directionVector: Vector2 = getDirectionVector()
+	#this code is for continuous
+	#accel = lerp(accel,3.0,10 * delta)
+	#velocity.x = lerp(velocity.x,directionVector.x * SPEED * accel,0.3)
+	#velocity.z = lerp(velocity.z,-directionVector.y * SPEED * accel,0.3)
 	
+	#this code is for impulse
+	velocity.x = directionVector.x * SPEED * dashCombo
+	velocity.z = -directionVector.y * SPEED * dashCombo
 
 
-func _physics_process(delta: float) -> void:
-	#below code for jumping
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+func decelerate(delta):
+	velocity.x = lerp(velocity.x,0.0,5 * delta)
+	velocity.z = lerp(velocity.z,0.0,5 * delta)
 
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+func rebound():
+	if get_wall_normal():
+		velocity.x += get_wall_normal().x * BOUNCE_MULTIPLIER
+		velocity.z += get_wall_normal().z * BOUNCE_MULTIPLIER
+
+func changeMouseInput(mouseClickInput):
+	if mouseClickInput == "Left Click":
+		mouseInput = "Right Click"
+	elif mouseClickInput == "Right Click":
+		mouseInput = "Left Click"
 
 
-	move_and_slide()
-	
+func _on_mouse_input_timer_timeout() -> void:
+	dashCooldown.start()
+	dashCombo = 1
